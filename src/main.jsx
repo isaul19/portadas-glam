2ab074286dcdb0592aa9032a0c45ce19706f522c
= <=>
2026-02-18 00:32:56 -0500

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import "./main.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <RouterProvider router={router} />
   </StrictMode>,
 );
